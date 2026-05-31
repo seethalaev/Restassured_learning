d35e265291a59df36a873fe156fe5336cc0c98eb
seethala e v <sheethalev@gmail.com>
2026-05-31 22:54:57 +0530

    given-for input details

--- src/Rest_Basics/Basics.java
+++ src/Rest_Basics/Basics.java
@@ -1,10 +1,40 @@
 package Rest_Basics;
 
+
+
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.RestAssured;
+
+
+
 public class Basics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Validate if Add place API is working as expected
+		
+		RestAssured.baseURI="https://rahulshettyacademy.com";
+		given().queryParam("key","qaclick123").header("Content-Type","application/json").body("{\r\n"
+				+ "  \"location\": {\r\n"
+				+ "    \"lat\": -38.383494,\r\n"
+				+ "    \"lng\": 33.427362\r\n"
+				+ "  },\r\n"
+				+ "  \"accuracy\": 50,\r\n"
+				+ "  \"name\": \"Frontline house\",\r\n"
+				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
+				+ "  \"types\": [\r\n"
+				+ "    \"shoe park\",\r\n"
+				+ "    \"shop\"\r\n"
+				+ "  ],\r\n"
+				+ "  \"website\": \"http://google.com\",\r\n"
+				+ "  \"language\": \"French-IN\"\r\n"
+				+ "}\r\n"
+				+ "")
+		
 	}
 
 }
