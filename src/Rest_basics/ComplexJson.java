d01d8ae5bca5e447c5520275405bce61506cb211
seethala e v <sheethalev@gmail.com>
2026-06-25 00:30:29 +0530

    /print no of copies sold by RPA course

--- src/Rest_basics/ComplexJson.java
+++ src/Rest_basics/ComplexJson.java
@@ -24,9 +24,18 @@ public class ComplexJson {
 		{
 			String courseTitle=js.get("courses["+i+"].title");
 			System.out.println(js.get("courses["+i+"].price").toString());
-			System.out.println(courseTitle);
-			
-			
+			System.out.println(courseTitle);	
+		}
+		//print no of copies sold by RPA course
+		for(int i=0;i<count;i++)
+		{
+			String courseTitle=js.get("courses["+i+"].title");
+			if(courseTitle.equalsIgnoreCase("RPA"))	
+			{
+				int copies=js.get("courses["+i+"].copies");
+				System.out.println(copies);
+				break;
+			}
 		}
 	}
 
