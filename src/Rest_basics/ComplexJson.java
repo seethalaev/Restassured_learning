3001f4d47913899cd9d2fd9f0e893f1790281593
seethala e v <sheethalev@gmail.com>
2026-06-25 00:19:27 +0530

    print all course title and their respective prices

--- src/Rest_basics/ComplexJson.java
+++ src/Rest_basics/ComplexJson.java
@@ -9,14 +9,25 @@ public class ComplexJson {
 		// TODO Auto-generated method stub	
 		JsonPath js=new JsonPath(Payload.CoursePrice());
 		//print no of courses returned by API
-		int coursesize=js.getInt("courses.size()");
-		System.out.println("course size is "+coursesize);
+		int count=js.getInt("courses.size()");
+		System.out.println("course size is "+count);
 		//print purchase amount
 		int purchaseamnt=js.getInt("dashboard.purchaseAmount");
 		System.out.println("purchase amount "+purchaseamnt);
 		//print title of first course
 		String titlefirstcourse=js.get("courses[0].title");
 		System.out.println("Title of first course-"+titlefirstcourse);
+		
+		
+		//print all course title and their respective prices
+		for(int i=0;i<count;i++)
+		{
+			String courseTitle=js.get("courses["+i+"].title");
+			System.out.println(js.get("courses["+i+"].price").toString());
+			System.out.println(courseTitle);
+			
+			
+		}
 	}
 
 }
